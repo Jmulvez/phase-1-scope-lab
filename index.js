9eecc04c10925a74b8be06ddcbd43c286af42824
John <johnmulvey48@yahoo.com>
2021-12-17 10:03:11 -0500

    git@github.com:Jmulvez/phase-1-scope-lab.git

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = 'Bob';
+
+function(upperCaseCustomerName); {
+    console.log(customerName);
+}
+
+function(setBestCustomer); {
+    console.log('not' + 'customerName');
+}
+
+function(overwriteBestCustomer); {
+    console.log(bestCustomer = 'Joe');
+}
+
+const leastFavoriteCustomer = Karen;
+
+function(changeLeastFavoriteCustomer); {
+    console.log(leastFavoriteCustomer = Timmy);
+}
